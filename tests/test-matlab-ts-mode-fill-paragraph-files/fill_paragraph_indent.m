60a40a097a15f231833f1d7aacc6f61588e22dc5
John Ciolfi <john.ciolfi.32@gmail.com>
2025-06-30 15:17:41 -0400

    matlab-ts-mode: add M-q fill paragraph tests

---
+++ tests/test-matlab-ts-mode-fill-paragraph-files/fill_paragraph_indent.m
@@ -0,0 +1,14 @@
+% -*- matlab-ts -*-
+function b = fill_paragraph_indent
+
+    % (t-utils-xr (set-fill-column 60) "C-n" "M-q")
+ if a > 2
+b= 1;
+      end
+s1 = "a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string a long string ";
+              disp(s1);
+
+   s2 = "foo"; % a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment  a long comment
+
+       disp(s2);
+end
