6711aefa42b680217d7565bd900d4e433c47ab6e
John Ciolfi <john.ciolfi.32@gmail.com>
2025-08-04 06:59:29 -0400

    matlab-ts-mode: removed an indent to do item

--- tests/test-matlab-ts-mode-indent-xr-files/indent_xr_mat4.m
+++ tests/test-matlab-ts-mode-indent-xr-files/indent_xr_mat4.m
@@ -1,4 +1,7 @@
 % -*- matlab-ts -*-
+%
+% See https://github.com/acristoffers/tree-sitter-matlab/issues/47
+%
 % (t-utils-xr "C-a" "C-n" "C-e" "C-m" (insert "[1, 2];\n") "C-i" (insert "[3, 4];") "C-m" (insert "];") "C-m")
 mat4 = [
 
