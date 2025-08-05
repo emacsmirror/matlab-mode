f0306d55b4f7c4e14377e69e41ed8236bd940df7
John Ciolfi <john.ciolfi.32@gmail.com>
2025-08-05 14:36:36 -0400

    treesit-mode-how-to.org: updated description of t-utils-xr

--- tests/test-matlab-ts-mode-indent-xr-files/indent_xr_cell1.m
+++ tests/test-matlab-ts-mode-indent-xr-files/indent_xr_cell1.m
@@ -1,2 +1,14 @@
 % -*- matlab-ts -*-
-% (t-utils-xr "C-a" "C-n" (insert "cell1 = { ...") "C-e" "C-m" (insert "{'one', ...") "C-m" (insert "'two'}, ...") "C-m" (insert "...") "C-m" (insert "{'three'}") "C-m" (insert "};") "C-m" (re-search-backward "^cell") (print (buffer-substring-no-properties (point) (point-max))))
+
+%{
+  (t-utils-xr
+
+  (re-search-forward "%}")  "C-n"
+  (insert "someVariable = {")      "C-m"
+  (insert "1234")                  "C-m"
+  (insert "};")                    "C-m"
+  (re-search-backward "^someVariable")
+  (t-utils-xr-print-code (point) (point-max))
+
+  )
+%}
