fa0fc18983afeeed9cea15a99860874acd783f85
John Ciolfi <john.ciolfi.32@gmail.com>
2025-08-06 07:38:23 -0400

    matlab-ts-mode: improve line-by-line indent testing

---
+++ tests/test-matlab-ts-mode-indent-files/indent_cont_statement_error_expected.m
@@ -0,0 +1,13 @@
+% -*- matlab-ts -*-
+%
+% t-utils-test-indent: no-line-by-line-indent - typing the incomplete expression line-by-line
+% results in a parse tree that can't match below.
+%
+function a=indent_cont_statement_error
+% The matlab-tree-sitter, by design for simplicity treats "..." as comments so the following
+% is an error in MATLAB, but doesn't generate a parse error and indents the same as if
+% the ellipsis (...) were present.
+    a =
+        1 +
+        2;
+end
