3f8c800e7ef59dfa8d4d460e1ed7b37e38012725
John Ciolfi <john.ciolfi.32@gmail.com>
2025-08-06 08:15:39 -0400

    matlab-ts-mode: remove skip.typing.txt for endless function indent test

--- tests/test-matlab-ts-mode-indent-files/indent_copyright_in_code.m
+++ tests/test-matlab-ts-mode-indent-files/indent_copyright_in_code.m
@@ -1,4 +1,8 @@
 % -*- matlab-ts -*-
+
+% t-utils-test-indent: no-line-by-line-indent - multiline comments cannot be indented correctly
+% when typing line-by-line because we don't know where in a multiline comment until we see the end.
+
    function b = indent_copyright_in_code
    %{
      sadf
--- tests/test-matlab-ts-mode-indent-files/indent_endless_function_expected.m
+++ tests/test-matlab-ts-mode-indent-files/indent_endless_function_expected.m
@@ -1,5 +1,9 @@
 % -*- matlab-ts -*-
-function indent_end_less_function(a)
+
+% t-utils-test-indent: no-line-by-line-indent - can't indent typing line-by-line
+% because on start, we assume an empty file has function indent level of 4
+
+function indent_endless_function(a)
 % function without a terminating end
 
 x = {'a', 'c()' 'b'};
