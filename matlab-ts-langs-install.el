b44b19fc4de7a1ec7700c12e11b19de515e2c744
John Ciolfi <john.ciolfi.32@gmail.com>
2025-09-13 20:36:06 -0400

    Add missing copyrights

--- matlab-ts-langs-install.el
+++ matlab-ts-langs-install.el
@@ -1,5 +1,20 @@
 ;;; matlab-ts-langs-install.el --- -*- lexical-binding: t -*-
 
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses>.
+
 ;;; Commentary:
 ;;
 ;; Download ~/.emacs.d/tree-sitter/libtree-sitter-LANGUAGES.so (or .dll or .dylib) from
