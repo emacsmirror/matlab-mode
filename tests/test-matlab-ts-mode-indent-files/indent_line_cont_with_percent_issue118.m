62614972be5f93f67edbf289a885e5c083902c3f
John Ciolfi <ciolfi@.mathworks.com>
2025-11-05 10:27:38 -0500

    matlab-ts-mode: add skipped test for matlab tree-sitter issue118

---
+++ tests/test-matlab-ts-mode-indent-files/indent_line_cont_with_percent_issue118.m
@@ -0,0 +1,8 @@
+% -*- matlab-ts -*-
+
+% See https://github.com/acristoffers/tree-sitter-matlab/issues/118
+
+c1 = {'foo'...               
+      ...           % comment1
+      'bar'...      % comment2
+     };
