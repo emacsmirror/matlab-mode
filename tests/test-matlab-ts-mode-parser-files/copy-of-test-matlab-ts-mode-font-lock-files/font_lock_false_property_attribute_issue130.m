5db1bbc6239f155e97052c2bed628fb2f1349c4b
John Ciolfi <ciolfi@.mathworks.com>
2025-11-18 19:26:25 -0500

    matlab-ts-mode: add test font_lock_false_property_attribute_issue130.m

---
+++ tests/test-matlab-ts-mode-parser-files/copy-of-test-matlab-ts-mode-font-lock-files/font_lock_false_property_attribute_issue130.m
@@ -0,0 +1,9 @@
+% -*- matlab-ts -*-
+
+% See https://github.com/acristoffers/tree-sitter-matlab/issues/130
+
+classdef font_lock_false_property_attribute_issue130
+    properties (~Constant)
+        A = 1;
+    end
+end
