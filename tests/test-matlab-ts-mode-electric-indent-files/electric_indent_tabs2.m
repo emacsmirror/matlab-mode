f587235135f6b34cf326958a32de1afff3ae58cb
John Ciolfi <ciolfi@.mathworks.com>
2026-01-21 12:14:35 -0500

    matlab-ts-mode--ei: add test to validate untabify doesn't touch tabs in strings

---
+++ tests/test-matlab-ts-mode-electric-indent-files/electric_indent_tabs2.m
@@ -0,0 +1,7 @@
+% -*- matlab-ts -*-
+
+% tabs within string and outside
+
+	s1 = "string with embeeded 		 tabs";				% foo
+
+	s2 = 'string with embeeded 		 tabs';				% foo
---
+++ tests/test-matlab-ts-mode-electric-indent-files/electric_indent_tabs2_expected_msgs.m
@@ -0,0 +1,7 @@
+% -*- matlab-ts -*- %  <{Matched rule: (matlab-ts-mode--i-top-level matlab-ts-mode--column-0 0)}>
+
+% tabs within string and outside %  <{Matched rule: (matlab-ts-mode--i-top-level matlab-ts-mode--column-0 0)}>
+
+s1 = "string with embeeded 		 tabs"; % foo %  <{Matched rule: (matlab-ts-mode--i-top-level matlab-ts-mode--column-0 0)}>
+
+s2 = 'string with embeeded 		 tabs'; % foo %  <{Matched rule: (matlab-ts-mode--i-top-level matlab-ts-mode--column-0 0)}>
