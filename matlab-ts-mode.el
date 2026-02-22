e6923314f3ffb291e45ece6b1ce08a0961e31adb
John Ciolfi <ciolfi@.mathworks.com>
2026-02-22 10:48:37 -0500

    NEWs.org: release 8.1.1

--- matlab-ts-mode.el
+++ matlab-ts-mode.el
@@ -1,6 +1,6 @@
 ;;; matlab-ts-mode.el --- MATLAB(R) Tree-Sitter Mode -*- lexical-binding: t -*-
 
-;; Version: 8.1.0
+;; Version: 8.1.1
 ;; URL: https://github.com/mathworks/Emacs-MATLAB-Mode
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
